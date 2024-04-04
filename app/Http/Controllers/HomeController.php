719735446e2d3a7f95100cf5802edcb5b4e5e05d
randi3281 <randiafif2312@gmail.com>
2024-04-04 07:28:44 +0700

    Refactor dropdown options in data_barang.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,16 +40,52 @@ class HomeController extends Controller
             if (jurnalhelper::cekkukis($kukis)) {
                 $usernya = user_manajemen::where('kukis', $_COOKIE['kukis'])->first();
                 if($menu == 'dashboard' ||  $menu == 'data_barang' ||$menu == 'user_manajemen' || $menu == 'mutasi' || $menu == 'pengguna' || $menu == 'penghapusan' || $menu == 'export_to_excel'){
-                    if($_COOKIE['posisi'] == 'pengguna' && $menu == 'user_manajemen'){
-                        return redirect()->route('dashboard', ['menu' => 'dashboard']);
-                    } else {
-                        if($menu == 'dashboard' || $menu == 'user_manajemen'){
+
+                    if($_COOKIE['posisi'] == 'pengguna'){
+
+                        if($menu == 'dashboard'){
+                            $datanya = user_manajemen::all();
+                            return view('dashboard', ['posisi' => $_COOKIE['posisi'], 'nama' => $_COOKIE['nama'], 'nik' => $_COOKIE['nik'], 'area' => $_COOKIE['area'], 'waktu' => $_COOKIE['current_time_formatted'], 'tanggal' => $_COOKIE['tanggal']], compact('menu'));
+                        }
+
+                        if($menu == 'data_barang'){
+                            return redirect()->route('dashboard', ['menu' => 'dashboard']);
+                        }
+
+                        if($menu == 'mutasi'){
+                            return redirect()->route('dashboard', ['menu' => 'dashboard']);
+                        }
+
+                        if($menu == 'penghapusan'){
+                            return redirect()->route('dashboard', ['menu' => 'dashboard']);
+                        }
+
+                        if($menu == 'export_to_excel'){
+                            return redirect()->route('dashboard', ['menu' => 'dashboard']);
+                        }
+
+                    } elseif($_COOKIE['posisi'] == 'admin'){
+
+                        if($menu == 'dashboard'){
+                            $datanya = user_manajemen::all();
+                            $_SESSION['data_barang_time'] = 'now';
+                            $_SESSION['data_barang_area']= 'all';
+                            $_SESSION['data_barang_bulan']= 'all';
+                            $_SESSION['data_barang_tahun']= 'all';
+                            return view('dashboard', ['posisi' => $_COOKIE['posisi'], 'nama' => $_COOKIE['nama'], 'nik' => $_COOKIE['nik'], 'area' => $_COOKIE['area'], 'waktu' => $_COOKIE['current_time_formatted'], 'tanggal' => $_COOKIE['tanggal'], 'menu' => $menu, 'datanya' => $datanya]);
+                        }
+
+                        if($menu == 'user_manajemen'){
                             $datanya = user_manajemen::paginate(10);
                             $_SESSION['data_barang_time'] = 'now';
                             $_SESSION['data_barang_area']= 'all';
                             $_SESSION['data_barang_bulan']= 'all';
                             $_SESSION['data_barang_tahun']= 'all';
-                        } elseif ($menu == 'data_barang' && $_COOKIE['posisi'] == 'admin'){
+                            return view('dashboard', ['posisi' => $_COOKIE['posisi'], 'nama' => $_COOKIE['nama'], 'nik' => $_COOKIE['nik'], 'area' => $_COOKIE['area'], 'waktu' => $_COOKIE['current_time_formatted'], 'tanggal' => $_COOKIE['tanggal'], 'menu' => $menu, 'datanya' => $datanya]);
+
+                        }
+
+                        if ($menu == 'data_barang'){
                             if($_SESSION['data_barang_time'] == 'now'){
                                 $datanya = data_barang_now::paginate(10);
                             } else {
@@ -61,7 +97,7 @@ class HomeController extends Controller
                                             $datanya = data_barang::where('tahun', $_SESSION['data_barang_tahun'])->paginate(10);
                                         } elseif($_SESSION['data_barang_tahun'] == 'all'){
                                             $datanya = data_barang::where('bulan', $_SESSION['data_barang_bulan'])->paginate(10);
-                                        }else{
+                                        }elseif($_SESSION['data_barang_bulan'] != 'all' && $_SESSION['data_barang_tahun'] != 'all'){
                                             $datanya = data_barang::where('tahun', $_SESSION['data_barang_tahun'])->where('bulan', $_SESSION['data_barang_bulan'])->paginate(10);
                                         }
                                     }elseif($_SESSION['data_barang_bulan'] == 'all'){
@@ -69,7 +105,7 @@ class HomeController extends Controller
                                             $datanya = data_barang::where('tahun', $_SESSION['data_barang_tahun'])->paginate(10);
                                         } elseif($_SESSION['data_barang_tahun'] == 'all'){
                                             $datanya = data_barang::where('area_user', $_SESSION['data_barang_area'])->paginate(10);
-                                        }else{
+                                        }elseif($_SESSION['data_barang_area'] != 'all' && $_SESSION['data_barang_tahun'] != 'all'){
                                             $datanya = data_barang::where('tahun', $_SESSION['data_barang_tahun'])->where('area_user', $_SESSION['data_barang_area'])->paginate(10);
                                         }
                                     }elseif($_SESSION['data_barang_tahun'] == 'all'){
@@ -77,25 +113,26 @@ class HomeController extends Controller
                                             $datanya = data_barang::where('bulan', $_SESSION['data_barang_bulan'])->paginate(10);
                                         } elseif($_SESSION['data_barang_bulan'] == 'all'){
                                             $datanya = data_barang::where('area_user', $_SESSION['data_barang_area'])->paginate(10);
-                                        }else{
+                                        }elseif($_SESSION['data_barang_area'] != 'all' && $_SESSION['data_barang_bulan'] != 'all'){
                                             $datanya = data_barang::where('bulan', $_SESSION['data_barang_bulan'])->where('area_user', $_SESSION['data_barang_area'])->paginate(10);
                                         }
+                                    }else{
+                                        $datanya = data_barang::where('bulan', $_SESSION['data_barang_bulan'])->where('area_user', $_SESSION['data_barang_area'])->where('tahun', $_SESSION['data_barang_tahun'])->paginate(10);
                                     }
-                                    $datanya = data_barang::where('bulan', $_SESSION['data_barang_bulan'])->where('area_user', $_SESSION['data_barang_area'])->where('tahun', $_SESSION['data_barang_tahun'])->paginate(10);
                                 }
+                                $data_user = user_manajemen::all();
+                                $data_barang_old = data_barang::all();
+                                return view('dashboard', ['posisi' => $_COOKIE['posisi'], 'nama' => $_COOKIE['nama'], 'nik' => $_COOKIE['nik'], 'area' => $_COOKIE['area'], 'waktu' => $_COOKIE['current_time_formatted'], 'tanggal' => $_COOKIE['tanggal'], 'menu' => $menu, 'datanya' => $datanya], compact('data_barang_old', 'data_user'));
                             }
                         }
-                        $data_user = user_manajemen::all();
-                        return view('dashboard', ['posisi' => $_COOKIE['posisi'], 'nama' => $_COOKIE['nama'], 'nik' => $_COOKIE['nik'], 'area' => $_COOKIE['area'], 'waktu' => $_COOKIE['current_time_formatted'], 'tanggal' => $_COOKIE['tanggal'], 'menu' => $menu, 'datanya' => $datanya, 'data_user' => $data_user]);
                     }
-                }else{
-                    return redirect()->route('dashboard', ['menu' => 'dashboard']);
                 }
+            }else{
+                return redirect()->route('dashboard', ['menu' => 'dashboard']);
             }
-        }
         return redirect()->route('index');
+        }
     }
-
     public function logout()
     {
         $usernya = user_manajemen::where('kukis', $_COOKIE['kukis'])->first();
--- resources/views/dashboard/admin/data_barang.blade.php
+++ resources/views/dashboard/admin/data_barang.blade.php
@@ -6,38 +6,77 @@
             <div class="form-group">
                 <label for="area"><b>Pilih Area : </b></label>
                 <select class="form-control-sm" id="area" name="area">
-                    <option value="all">All</option>
-                    @foreach ($data_user as $item)
-                        <option value="{{ $item->area }}" name="{{ $item->area }}">{{ $item->area }}
-                        </option>
+                    <option value="{{ $_SESSION['data_barang_area'] }}" name="{{ $_SESSION['data_barang_area'] }}">
+                        @php
+                            $cek_area = [];
+                            $cek_area[] = $_SESSION['data_barang_area'];
+                        @endphp
+                        {{ $_SESSION['data_barang_area'] }}
+                        @foreach ($data_user as $item)
+                            @if (!in_array($item->area, $cek_area))
+                                @php
+                                    $cek_area[] = $item->area;
+                                @endphp
+                    <option value="{{ $item->area }}" name="{{ $item->area }}">{{ $item->area }} </option>
+                    @endif
+                    @if (!in_array('all', $cek_area))
+                        @php
+                            $cek_area[] = 'all';
+                        @endphp
+                        <option value="all" name="all">all</option>
+                    @endif
                     @endforeach
                 </select>
                 <label for="bulan" class="ms-2"><b>Bulan : </b></label>
                 <select class="form-control-sm" id="bulan" name="bulan">
-                    <option value="all">All</option>
-                    <option value="Januari">Januari</option>
-                    <option value="Februari">Februari</option>
-                    <option value="Maret">Maret</option>
-                    <option value="April">April</option>
-                    <option value="Mei">Mei</option>
-                    <option value="Juni">Juni</option>
-                    <option value="Juli">Juli</option>
-                    <option value="Agustus">Agustus</option>
-                    <option value="September">September</option>
-                    <option value="Oktober">Oktober</option>
-                    <option value="November">November</option>
-                    <option value="Desember">Desember</option>
+                    <option value="{{ $_SESSION['data_barang_bulan'] }}" name="{{ $_SESSION['data_barang_bulan'] }}">
+                        @php
+                            $cek_bulan = [];
+                            $cek_bulan[] = $_SESSION['data_barang_bulan'];
+                        @endphp
+                        {{ $_SESSION['data_barang_bulan'] }}
+                        @foreach ($data_barang_old as $item)
+                            @if (!in_array($item->bulan, $cek_bulan))
+                                @php
+                                    $cek_bulan[] = $item->bulan;
+                                @endphp
+                    <option value="{{ $item->bulan }}" name="{{ $item->bulan }}">{{ $item->bulan }} </option>
+                    @endif
+                    @if (!in_array('all', $cek_bulan))
+                        @php
+                            $cek_bulan[] = 'all';
+                        @endphp
+                        <option value="all">all</option>
+                    @endif
+                    @endforeach
                 </select>
-                <label for="bulan" class="ms-2"><b>Tahun : </b></label>
+                <label for="tahun" class="ms-2"><b>Tahun : </b></label>
                 <select class="form-control-sm" id="tahun" name="tahun">
-                    <option value="all">All</option>
-                    <option value="2023" name="2023">2023</option>
-                    <option value="2024" name="2024">2024</option>
+                    <option value="{{ $_SESSION['data_barang_tahun'] }}" name="{{ $_SESSION['data_barang_tahun'] }}">
+                        @php
+                            $cek_tahun = [];
+                            $cek_tahun[] = $_SESSION['data_barang_tahun'];
+                        @endphp
+                        {{ $_SESSION['data_barang_tahun'] }}
+                        @foreach ($data_barang_old as $item)
+                            @if (!in_array($item->tahun, $cek_tahun))
+                                @php
+                                    $cek_tahun[] = $item->tahun;
+                                @endphp
+                    <option value="{{ $item->tahun }}" name="{{ $item->tahun }}">{{ $item->tahun }} </option>
+                    @endif
+                    @if (!in_array('all', $cek_tahun))
+                        @php
+                            $cek_tahun[] = 'all';
+                        @endphp
+                        <option value="all">all</option>
+                    @endif
+                    @endforeach
                 </select>
                 <button type="submit" class="btn btn-primary btn ms-3"><b>Submit</b></button>
             </div>
         </form>
-        <p style="font-size: 14px" class="mb-4"> <b>Keterangan : <i></b>Area : {{ $_SESSION['data_barang_area'] }},
+        <p style="font-size: 14px" class="mb-4"> <b>Keterangan : <i></b>Area : {{ $_SESSION['data_barang_bulan'] }},
             Bulan :
             {{ $_SESSION['data_barang_bulan'] }}, Tahun
             :
